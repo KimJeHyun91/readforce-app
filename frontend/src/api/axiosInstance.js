4c697587580147bb83fec36cabc4dca3c58c8ea7
KimJeHyun91 <kjh910920@gmail.com>
2025-07-17 23:59:24 +0900

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://readforce-app-backend-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
